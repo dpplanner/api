249ba538b254b51657d07cdd3d0676a911f6fafb
impala <dla4051@naver.com>
2023-07-31 18:32:46 +0900

    Member: @Builder 어노테이션을 통해 빌더패턴 적용

--- dplanner/src/main/java/com/dp/dplanner/domain/Member.java
+++ dplanner/src/main/java/com/dp/dplanner/domain/Member.java
@@ -3,10 +3,14 @@ package com.dp.dplanner.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     @GeneratedValue
@@ -15,4 +19,9 @@ public class Member {
     private String name;
     private String info;
 
+    @Builder
+    public Member(String name, String info) {
+        this.name = name;
+        this.info = info;
+    }
 }
