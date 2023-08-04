32e849f84884e1a7c13c211bd13418d9907aadcf
impala <dla4051@naver.com>
2023-08-04 19:55:08 +0900

    feature : 클럽 회원을 승인 여부로 나누어 조회하는 기능 추가(findAllConfirmedClubMemberByClub, findAllUnConfirmedClubMemberByClub)

--- dplanner/src/main/java/com/dp/dplanner/repository/ClubMemberRepository.java
+++ dplanner/src/main/java/com/dp/dplanner/repository/ClubMemberRepository.java
@@ -4,6 +4,8 @@ import com.dp.dplanner.domain.Member;
 import com.dp.dplanner.domain.club.Club;
 import com.dp.dplanner.domain.club.ClubMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +18,12 @@ public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
     Optional<ClubMember> findByClubIdAndMemberId(Long clubId, Long memberId);
 
     List<ClubMember> findAllByClub(Club club);
+
+
+    @Query(value = "select cm from ClubMember cm where cm.club = :club and cm.isConfirmed = true")
+    List<ClubMember> findAllConfirmedClubMemberByClub(@Param("club") Club club);
+
+    @Query(value = "select cm from ClubMember cm where cm.club = :club and cm.isConfirmed = false")
+    List<ClubMember> findAllUnConfirmedClubMemberByClub(@Param("club") Club club);
 
 }
