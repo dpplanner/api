6f1aeb098750da9c568c35b3568159da54a8983b
impala <dla4051@naver.com>
2023-08-04 19:55:54 +0900

    test : 권한 여부에 따른 클럽 회원 조회 테스트 추가

--- dplanner/src/test/java/com/dp/dplanner/service/ClubMemberServiceTests.java
+++ dplanner/src/test/java/com/dp/dplanner/service/ClubMemberServiceTests.java
@@ -2,6 +2,8 @@ package com.dp.dplanner.service;
 
 import com.dp.dplanner.domain.Member;
 import com.dp.dplanner.domain.club.Club;
+import com.dp.dplanner.domain.club.ClubAuthority;
+import com.dp.dplanner.domain.club.ClubAuthorityType;
 import com.dp.dplanner.domain.club.ClubMember;
 import com.dp.dplanner.dto.ClubMemberDto;
 import com.dp.dplanner.repository.ClubMemberRepository;
@@ -54,11 +56,43 @@ public class ClubMemberServiceTests {
     @DisplayName("사용자는 같은 클럽에 속한 회원들의 정보를 조회할 수 있다.")
     public void findMyClubMembers() throws Exception {
         //given
-        ClubMember clubMember = createClubMember(club, member);
+        ClubMember clubMember = createConfirmedClubMember(club, member);
         given(clubMemberRepository.findByClubIdAndMemberId(clubId, memberId)).willReturn(Optional.ofNullable(clubMember));
 
-        List<ClubMember> clubMembers = createClubMembers(club, 3);
+        List<ClubMember> clubMembers = createConfirmedClubMembers(club, 3);
         clubMembers.add(clubMember); //clubMembers = [member, clubMember0, clubMember1, clubMember2]
+        given(clubMemberRepository.findAllConfirmedClubMemberByClub(club)).willReturn(clubMembers);
+
+        ClubMember unConfirmedMember = createClubMember(club, createMember("unConfirmedMember"));
+        ClubMember otherClubMember = createClubMember(createClub("otherClub"), createMember("otherClubMember"));
+
+        //when
+        List<ClubMemberDto.Response> findClubMembers = clubMemberService.findMyClubMembers(clubId, memberId);
+
+        //then
+        assertThat(findClubMembers).as("결과가 존재해야 함").isNotNull();
+
+        List<String> findMemberNames = findClubMembers.stream().map(ClubMemberDto.Response::getName).toList();
+        assertThat(findMemberNames).as("결과에 본인이 포함되어야 함").contains(member.getName());
+        assertThat(findMemberNames).as("결과에 같은 클럽의 다른 회원들이 포함되어야 함")
+                .contains("clubMember0", "clubMember1", "clubMember2");
+        assertThat(findMemberNames).as("다른 클럽의 회원은 포함되지 않아야 함")
+                .doesNotContain("otherClubMember");
+        assertThat(findMemberNames).as("결과에 승인되지 않은 회원은 포함되지 않아야 함")
+                .doesNotContain("unConfirmedMember");
+    }
+
+    @Test
+    @DisplayName("관리자는 승인되지 않은 회원을 포함한 전체 클럽 회원을 조회할 수 있다.")
+    public void findMyClubMemberByAdmin() throws Exception {
+        //given
+        ClubMember clubMember = createConfirmedClubMember(club, member);
+        clubMember.setAdmin();
+        given(clubMemberRepository.findByClubIdAndMemberId(clubId, memberId)).willReturn(Optional.ofNullable(clubMember));
+
+        List<ClubMember> clubMembers = prepareClubMembersIncludeUnConfirmedMember("unConfirmedMember");
+        clubMembers.add(clubMember); //clubMembers = [member, clubMember0, clubMember1, clubMember2]
+
         given(clubMemberRepository.findAllByClub(club)).willReturn(clubMembers);
 
         //when
@@ -68,11 +102,35 @@ public class ClubMemberServiceTests {
         assertThat(findClubMembers).as("결과가 존재해야 함").isNotNull();
 
         List<String> findMemberNames = findClubMembers.stream().map(ClubMemberDto.Response::getName).toList();
-        assertThat(findMemberNames).as("결과에 본인이 포함되어야 함").contains("member");
-        assertThat(findMemberNames).as("결과에 같은 클럽의 다른 회원들이 포함되어야 함")
-                .contains("clubMember0", "clubMember1", "clubMember2");
-        assertThat(findMemberNames).as("다른 클럽의 회원은 포함되지 않아야 함")
-                .doesNotContain("clubMember3", "clubMember4");
+        assertThat(findMemberNames).as("결과에 승인되지 않은 회원이 포함되어야 함")
+                .contains("unConfirmedMember");
+    }
+
+    @Test
+    @DisplayName("매니저에게 클럽 회원 관리 권한이 있으면 승인되지 않은 회원을 포함한 전체 회원을 조회할 수 있다.")
+    public void findMyClubMemberByManagerHasMEMBER_ALL() throws Exception {
+        //given
+        ClubAuthority.createAuthorities(club, List.of(ClubAuthorityType.MEMBER_ALL));
+
+        ClubMember clubMember = createConfirmedClubMember(club, member);
+        clubMember.setManager();
+
+        given(clubMemberRepository.findByClubIdAndMemberId(clubId, memberId)).willReturn(Optional.ofNullable(clubMember));
+
+        List<ClubMember> clubMembers = prepareClubMembersIncludeUnConfirmedMember("unConfirmedMember");
+        clubMembers.add(clubMember); //clubMembers = [member, clubMember0, clubMember1, clubMember2]
+
+        given(clubMemberRepository.findAllByClub(club)).willReturn(clubMembers);
+
+        //when
+        List<ClubMemberDto.Response> findClubMembers = clubMemberService.findMyClubMembers(clubId, memberId);
+
+        //then
+        assertThat(findClubMembers).as("결과가 존재해야 함").isNotNull();
+
+        List<String> findMemberNames = findClubMembers.stream().map(ClubMemberDto.Response::getName).toList();
+        assertThat(findMemberNames).as("결과에 승인되지 않은 회원이 포함되어야 함")
+                .contains("unConfirmedMember");
     }
     
     @Test
@@ -88,6 +146,19 @@ public class ClubMemberServiceTests {
                 .isInstanceOf(NoSuchElementException.class);
     }
 
+    @Test
+    @DisplayName("관리자는 일반 회원을 매니저로 설정할 수 있다.")
+    public void makeUserToManagerByAdmin() throws Exception {
+        //given
+        ClubMember admin = createConfirmedClubMember(club, member);
+        admin.setAdmin();
+
+        ClubMember clubMember = createClubMember(club, createMember("clubMember"));
+
+        //when
+
+        //then
+    }
 
     /**
      * Member util method
@@ -111,14 +182,20 @@ public class ClubMemberServiceTests {
     /**
      * ClubMember util method
      */
-    private static List<ClubMember> createClubMembers(Club club, int n) {
+    private static List<ClubMember> createConfirmedClubMembers(Club club, int n) {
         List<ClubMember> clubMembers = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             Member anotherMember = createMember("clubMember" + i);
-            ClubMember anotherClubMember = createClubMember(club, anotherMember);
+            ClubMember anotherClubMember = createConfirmedClubMember(club, anotherMember);
             clubMembers.add(anotherClubMember);
         }
         return clubMembers;
+    }
+
+    private static ClubMember createConfirmedClubMember(Club club, Member member) {
+        ClubMember clubMember = createClubMember(club, member);
+        clubMember.confirm();
+        return clubMember;
     }
 
     private static ClubMember createClubMember(Club club, Member member) {
@@ -126,5 +203,14 @@ public class ClubMemberServiceTests {
                 .club(club)
                 .member(member)
                 .build();
+    }
+
+    private List<ClubMember> prepareClubMembersIncludeUnConfirmedMember(String unConfirmedMemberName) {
+        List<ClubMember> clubMembers = createConfirmedClubMembers(club, 3);
+
+        ClubMember unConfirmedMember = createClubMember(club, createMember(unConfirmedMemberName));
+        clubMembers.add(unConfirmedMember);
+
+        return clubMembers;
     }
 }
