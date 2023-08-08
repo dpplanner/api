534d5d6ee4ce184192cd33dc5c2e091080cb9c32
정찬영 <lms062111@naver.com>
2023-08-08 23:15:44 +0900

    test : Attachment CRUD 기능 테스트 코드

---
+++ dplanner/src/test/java/com/dp/dplanner/repository/AttachmentRepositoryTest.java
@@ -0,0 +1,101 @@
+package com.dp.dplanner.repository;
+
+import com.dp.dplanner.domain.Attachment;
+import com.dp.dplanner.domain.FileType;
+import com.dp.dplanner.domain.Member;
+import com.dp.dplanner.domain.Post;
+import com.dp.dplanner.domain.club.Club;
+import com.dp.dplanner.domain.club.ClubMember;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class AttachmentRepositoryTest {
+
+    @Autowired
+    private AttachmentRepository attachmentRepository;
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    Member member;
+    Club club;
+    ClubMember clubMember;
+    Post post;
+    Long postId;
+    @BeforeEach
+    public void setUp() {
+        member = Member.builder().build();
+        club = Club.builder().build();
+        clubMember = ClubMember.builder().club(club).member(member).name("test").build();
+        post = Post.builder().clubMember(clubMember).club(club).build();
+
+        testEntityManager.persist(member);
+        testEntityManager.persist(club);
+        testEntityManager.persist(clubMember);
+        testEntityManager.persist(post);
+    }
+    @Test
+    public void AttachmentRepository_CreateAttachment_ReturnAttachment() {
+
+        Attachment attachment = createAttachment("test");
+
+        Attachment save = attachmentRepository.save(attachment);
+
+        assertThat(save).isNotNull();
+        assertThat(save.getId()).isGreaterThan(0L);
+        assertThat(save.getUrl()).isEqualTo("test");
+        assertThat(save.getType()).isEqualTo(FileType.IMAGE);
+
+        assertThat(post.getAttachments().size()).isEqualTo(1);
+        assertThat(post.getAttachments().get(0)).isEqualTo(save);
+
+
+    }
+
+    @Test
+    public void AttachmentRepository_findByPostId_ReturnAttachments() {
+
+        Attachment attachment = createAttachment("test");
+        Attachment attachment2 = createAttachment("test2");
+
+        postId = post.getId();
+        attachmentRepository.save(attachment);
+        attachmentRepository.save(attachment2);
+
+        List<Attachment> attachments = attachmentRepository.findAttachmentsByPostId(postId);
+
+        assertThat(attachments.size()).isEqualTo(2);
+        assertThat(attachments).extracting(Attachment::getPost).extracting(Post::getId).containsOnly(postId);
+        assertThat(post.getAttachments().size()).isEqualTo(2);
+    }
+
+    @Test
+    public void AttachmentRepository_deleteAttachment_Return(){
+        Attachment attachment = createAttachment("test");
+        attachmentRepository.save(attachment);
+
+        attachmentRepository.delete(attachment);
+
+        Optional<Attachment> deletedAttachment = attachmentRepository.findById(attachment.getId());
+        assertThat(deletedAttachment).isEmpty();
+
+    }
+    private Attachment createAttachment(String url) {
+        return Attachment.builder()
+                .url(url)
+                .post(post)
+                .type(FileType.IMAGE)
+                .build();
+    }
+}
---
+++ dplanner/src/test/java/com/dp/dplanner/service/AttachmentServiceTest.java
@@ -0,0 +1,103 @@
+package com.dp.dplanner.service;
+
+import com.dp.dplanner.domain.Attachment;
+import com.dp.dplanner.domain.FileType;
+import com.dp.dplanner.domain.Post;
+import com.dp.dplanner.repository.AttachmentRepository;
+import com.dp.dplanner.repository.PostRepository;
+import com.dp.dplanner.service.upload.UploadService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static com.dp.dplanner.dto.AttachmentDto.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AttachmentServiceTest {
+
+    @Mock
+    private AttachmentRepository attachmentRepository;
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private UploadService uploadService;
+
+    @InjectMocks
+    private AttachmentService attachmentService;
+
+    Post post;
+    Long postId;
+
+    @BeforeEach
+    public void setUp() {
+        post = Post.builder().build();
+        postId = 10L;
+        ReflectionTestUtils.setField(post, "id", postId);
+    }
+    @Test
+    public void AttachmentService_CreateAttachment_ReturnListResponseDto() throws Exception {
+
+        String fileName = "testUpload";
+        String contentType = "jpg";
+        String filePath = "src/test/resources/test/testUpload.jpg";
+        String savedPath = "src/main/resources/test/save/" + fileName + "." + contentType;
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        MockMultipartFile multipartFile = new MockMultipartFile(fileName, fileName + "." + contentType, contentType, fileInputStream);
+
+        Create createDto = Create.builder()
+                .postId(postId)
+                .files(Arrays.asList(multipartFile))
+                .build();
+
+        when(postRepository.findById(postId)).thenReturn(Optional.ofNullable(post));
+        when(uploadService.uploadFile(multipartFile)).thenReturn(savedPath);
+        when(uploadService.getFileType(savedPath)).thenReturn(FileType.IMAGE);
+        when(attachmentRepository.save(Mockito.any(Attachment.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        List<Response> responseList = attachmentService.createAttachment(createDto);
+
+        Response create = responseList.get(0);
+
+        assertThat(create.getPostId()).isEqualTo(postId);
+        assertThat(create.getUrl()).isEqualTo("src/main/resources/test/save/testUpload.jpg");
+        assertThat(create.getType()).isEqualTo(FileType.IMAGE);
+        assertThat(post.getAttachments().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void AttachmentService_getAttachmentsByPostId_ReturnListResponseDto() {
+
+        Attachment attachment = Attachment.builder()
+                .post(post)
+                .type(FileType.IMAGE)
+                .url("url1")
+                .build();
+
+        Attachment attachment2 = Attachment.builder()
+                .post(post)
+                .type(FileType.IMAGE)
+                .url("url2")
+                .build();
+
+        when(postRepository.findById(postId)).thenReturn(Optional.ofNullable(post));
+        when(attachmentRepository.findAttachmentsByPostId(postId)).thenReturn(Arrays.asList(attachment, attachment2));
+
+        List<Response> responseList = attachmentService.getAttachmentsByPostId(postId);
+
+        assertThat(responseList.size()).isEqualTo(2);
+        assertThat(responseList).extracting(Response::getPostId).containsOnly(postId);
+    }
+}
