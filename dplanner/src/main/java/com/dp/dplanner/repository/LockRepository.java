79de0c057efcb61d91a63f095476fb9fe25702f3
정찬영 <lms062111@naver.com>
2023-08-11 01:00:45 +0900

    fix : 테스트 케이스에 통과하도록 fix

--- dplanner/src/main/java/com/dp/dplanner/repository/LockRepository.java
+++ dplanner/src/main/java/com/dp/dplanner/repository/LockRepository.java
@@ -13,8 +13,14 @@ public interface LockRepository extends JpaRepository<Lock, Long> {
     @Query("select l " +
             "from Lock l " +
             "where l.resource.id = :resourceId " +
-            "and (( l.period.startDateTime <= :start and :start <= l.period.endDateTime) or (:start <= l.period.startDateTime AND l.period.startDateTime <= :end )) " +
+            "and (( l.period.startDateTime <= :start and :start < l.period.endDateTime) or (:start <= l.period.startDateTime AND l.period.startDateTime < :end )) " +
             "order by l.period.startDateTime asc , l.period.endDateTime asc")
     List<Lock> findLocksBetween(@Param(value = "start") LocalDateTime start, @Param(value = "end") LocalDateTime end,@Param(value = "resourceId") Long resourceId);
 
+    @Query("select exists (select l " +
+            "from Lock l " +
+            "where l.resource.id = :resourceId " +
+            "and (( l.period.startDateTime <= :start and :start < l.period.endDateTime) or (:start <= l.period.startDateTime AND l.period.startDateTime < :end )) " +
+            "order by l.period.startDateTime asc , l.period.endDateTime asc)")
+    boolean existsLocksBetween(@Param(value = "start") LocalDateTime start, @Param(value = "end") LocalDateTime end,@Param(value = "resourceId") Long resourceId);
 }
