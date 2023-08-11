8c909d605d82f66bba736d68008aa5eee7cfddc3
impala <dla4051@naver.com>
2023-08-11 19:18:08 +0900

    test : ReservationService updateReservation 테스트 코드 작성

--- dplanner/src/test/java/com/dp/dplanner/service/ReservationServiceTests.java
+++ dplanner/src/test/java/com/dp/dplanner/service/ReservationServiceTests.java
@@ -1,6 +1,7 @@
 package com.dp.dplanner.service;
 
 import com.dp.dplanner.domain.Member;
+import com.dp.dplanner.domain.Period;
 import com.dp.dplanner.domain.Reservation;
 import com.dp.dplanner.domain.Resource;
 import com.dp.dplanner.domain.club.Club;
@@ -79,15 +80,8 @@ public class ReservationServiceTests {
         given(reservationRepository.save(any(Reservation.class))).willAnswer(invocation -> invocation.getArgument(0));
 
         //when
-        ReservationDto.Create createDto = ReservationDto.Create.builder()
-                .resourceId(resourceId)
-                .title("reservation")
-                .usage("usage")
-                .sharing(false)
-                .startDateTime(LocalDateTime.of(2023, 8, 10, 20, 0))
-                .endDateTime(LocalDateTime.of(2023, 8, 10, 21, 0))
-                .build();
-
+        ReservationDto.Create createDto = getCreateDto(
+                resourceId, "reservation", "usage", false, getTime(20), getTime(21));
         ReservationDto.Response responseDto = reservationService.createReservation(clubMemberId, createDto);
 
         //then
@@ -113,12 +107,8 @@ public class ReservationServiceTests {
         clubMember.setAdmin();
 
         //when
-        ReservationDto.Create createDto = ReservationDto.Create.builder()
-                .resourceId(resourceId)
-                .startDateTime(LocalDateTime.of(2023, 8, 10, 20, 0))
-                .endDateTime(LocalDateTime.of(2023, 8, 10, 21, 0))
-                .build();
-
+        ReservationDto.Create createDto = getCreateDto(
+                resourceId, "reservation", "usage", false, getTime(20), getTime(21));
         ReservationDto.Response responseDto = reservationService.createReservation(clubMemberId, createDto);
 
         //then
@@ -137,12 +127,8 @@ public class ReservationServiceTests {
         clubMember.setManager();
 
         //when
-        ReservationDto.Create createDto = ReservationDto.Create.builder()
-                .resourceId(resourceId)
-                .startDateTime(LocalDateTime.of(2023, 8, 10, 20, 0))
-                .endDateTime(LocalDateTime.of(2023, 8, 10, 21, 0))
-                .build();
-
+        ReservationDto.Create createDto = getCreateDto(
+                resourceId, "reservation", "usage", false, getTime(20), getTime(21));
         ReservationDto.Response responseDto = reservationService.createReservation(clubMemberId, createDto);
 
         //then
@@ -157,11 +143,8 @@ public class ReservationServiceTests {
 
         //when
         //then
-        ReservationDto.Create createDto = ReservationDto.Create.builder()
-                .resourceId(resourceId)
-                .startDateTime(LocalDateTime.of(2023, 8, 10, 20, 0))
-                .endDateTime(LocalDateTime.of(2023, 8, 10, 21, 0))
-                .build();
+        ReservationDto.Create createDto = getCreateDto(
+                resourceId, "reservation", "usage", false, getTime(20), getTime(21));
 
         assertThatThrownBy(() -> reservationService.createReservation(clubMemberId, createDto))
                 .isInstanceOf(IllegalStateException.class);
@@ -175,11 +158,8 @@ public class ReservationServiceTests {
 
         //when
         //then
-        ReservationDto.Create createDto = ReservationDto.Create.builder()
-                .resourceId(resourceId)
-                .startDateTime(LocalDateTime.of(2023, 8, 10, 20, 0))
-                .endDateTime(LocalDateTime.of(2023, 8, 10, 21, 0))
-                .build();
+        ReservationDto.Create createDto = getCreateDto(
+                resourceId, "reservation", "usage", false, getTime(20), getTime(21));
 
         assertThatThrownBy(() -> reservationService.createReservation(clubMemberId, createDto))
                 .isInstanceOf(IllegalStateException.class);
@@ -194,11 +174,8 @@ public class ReservationServiceTests {
 
         //when
         //then
-        ReservationDto.Create createDto = ReservationDto.Create.builder()
-                .resourceId(resourceId)
-                .startDateTime(LocalDateTime.of(2023, 8, 10, 20, 0))
-                .endDateTime(LocalDateTime.of(2023, 8, 10, 21, 0))
-                .build();
+        ReservationDto.Create createDto = getCreateDto(
+                resourceId, "reservation", "usage", false, getTime(20), getTime(21));
 
         assertThatThrownBy(() -> reservationService.createReservation(otherClubMemberId, createDto))
                 .isInstanceOf(IllegalStateException.class);
@@ -212,11 +189,8 @@ public class ReservationServiceTests {
 
         //when
         //then
-        ReservationDto.Create createDto = ReservationDto.Create.builder()
-                .resourceId(resourceId)
-                .startDateTime(LocalDateTime.of(2023, 8, 10, 20, 0))
-                .endDateTime(LocalDateTime.of(2023, 8, 10, 21, 0))
-                .build();
+        ReservationDto.Create createDto = getCreateDto(
+                resourceId, "reservation", "usage", false, getTime(20), getTime(21));
 
         assertThatThrownBy(() -> reservationService.createReservation(clubMemberId, createDto))
                 .isInstanceOf(NoSuchElementException.class);
@@ -231,14 +205,235 @@ public class ReservationServiceTests {
 
         //when
         //then
-        ReservationDto.Create createDto = ReservationDto.Create.builder()
-                .resourceId(resourceId)
-                .startDateTime(LocalDateTime.of(2023, 8, 10, 20, 0))
-                .endDateTime(LocalDateTime.of(2023, 8, 10, 21, 0))
-                .build();
+        ReservationDto.Create createDto = getCreateDto(
+                resourceId, "reservation", "usage", false, getTime(20), getTime(21));
 
         assertThatThrownBy(() -> reservationService.createReservation(clubMemberId, createDto))
                 .isInstanceOf(NoSuchElementException.class);
     }
+    
+    @Test
+    @DisplayName("사용자는 본인의 예약을 수정할 수 있다.")
+    public void updateReservationByUser() throws Exception {
+        //given
+        Long reservationId = 1L;
+        Reservation reservation = createReservation(
+                resource, clubMember, getPeriod(20, 21), "title", "usage", false);
+        given(reservationRepository.findById(reservationId)).willReturn(Optional.ofNullable(reservation));
 
+        //when
+        ReservationDto.Update updateDto = getUpdateDto(
+                reservationId, resourceId, "newTitle", "newUsage",
+                false, getTime(20), getTime(22)
+        );
+
+        ReservationDto.Response responseDto = reservationService.updateReservation(clubMemberId, updateDto);
+        
+        //then
+        assertThat(responseDto).as("결과가 존재해야 한다").isNotNull();
+        assertThat(responseDto.getClubMemberId()).as("예약자 정보가 일치해야 한다").isEqualTo(clubMemberId);
+        assertThat(responseDto.getResourceId()).as("예약한 리소스 정보가 일치해야 한다").isEqualTo(resourceId);
+        assertThat(responseDto.getTitle()).as("예약 제목이 일치해야 한다").isEqualTo(updateDto.getTitle());
+        assertThat(responseDto.getUsage()).as("예약 용도가 일치해야 한다").isEqualTo(updateDto.getUsage());
+        assertThat(responseDto.isSharing()).as("공유 여부가 일치해야 한다").isEqualTo(updateDto.isSharing());
+        assertThat(responseDto.getStartDateTime()).as("예약 시작 시간이 일치해야 한다").isEqualTo(updateDto.getStartDateTime());
+        assertThat(responseDto.getEndDateTime()).as("예약 종료 시간이 일치해야 한다").isEqualTo(updateDto.getEndDateTime());
+        assertThat(responseDto.isConfirmed()).as("예약은 승인 대기상태여야 한다").isFalse();
+    }
+
+    @Test
+    @DisplayName("사용자가 승인된 예약을 수정하면 승인 대기상태로 전환된다.")
+    public void updateConfirmedReservationByUserThenNotConfirmed() throws Exception {
+        //given
+        Long reservationId = 1L;
+        Reservation reservation = createReservation(
+                resource, clubMember, getPeriod(20, 21), "title", "usage", false);
+        reservation.confirm();
+        given(reservationRepository.findById(reservationId)).willReturn(Optional.ofNullable(reservation));
+
+        //when
+        ReservationDto.Update updateDto = getUpdateDto(
+                reservationId, resourceId, "newTitle", "newUsage",
+                false, getTime(20), getTime(22)
+        );
+
+        ReservationDto.Response responseDto = reservationService.updateReservation(clubMemberId, updateDto);
+
+        //then
+        assertThat(responseDto.isConfirmed()).as("예약은 승인 대기상태여야 한다").isFalse();
+    }
+
+    @Test
+    @DisplayName("관리자가 승인된 예약을 수정시 승인대기를 하지 않는다")
+    public void updateReservationByAdminThenConfirmed() throws Exception {
+        //given
+        clubMember.setAdmin();
+
+        Long reservationId = 1L;
+        Reservation reservation = createReservation(
+                resource, clubMember, getPeriod(20, 21), "title", "usage", false);
+        reservation.confirm();
+        given(reservationRepository.findById(reservationId)).willReturn(Optional.ofNullable(reservation));
+
+        //when
+        ReservationDto.Update updateDto = getUpdateDto(
+                reservationId, resourceId, "newTitle", "newUsage",
+                false, getTime(20), getTime(22)
+        );
+
+        ReservationDto.Response responseDto = reservationService.updateReservation(clubMemberId, updateDto);
+
+        //then
+        assertThat(responseDto.isConfirmed()).as("예약은 승인 완료상태여야 한다").isTrue();
+    }
+
+    @Test
+    @DisplayName("권한이 있는 매니저가 승인된 예약을 수정시 승인대기를 하지 않는다")
+    public void updateReservationByManagerHasSCHEDULE_ALLThenConfirmed() throws Exception {
+        //given
+        ClubAuthority.createAuthorities(clubMember.getClub(), List.of(ClubAuthorityType.SCHEDULE_ALL));
+        clubMember.setManager();
+
+        Long reservationId = 1L;
+        Reservation reservation = createReservation(
+                resource, clubMember, getPeriod(20, 21), "title", "usage", false);
+        reservation.confirm();
+        given(reservationRepository.findById(reservationId)).willReturn(Optional.ofNullable(reservation));
+
+        //when
+        ReservationDto.Update updateDto = getUpdateDto(
+                reservationId, resourceId, "newTitle", "newUsage",
+                false, getTime(20), getTime(22)
+        );
+
+        ReservationDto.Response responseDto = reservationService.updateReservation(clubMemberId, updateDto);
+
+        //then
+        assertThat(responseDto.isConfirmed()).as("예약은 승인 완료상태여야 한다").isTrue();
+    }
+
+    @Test
+    @DisplayName("예약 시간 수정시 다른 예약이 있으면 IllegalStateException")
+    public void updateReservationWhenOverlappedWithOtherReservationThenException() throws Exception {
+        //given
+        Long reservationId = 1L;
+        given(reservationRepository.existsOthersBetween(any(), any(), eq(resourceId), eq(reservationId)))
+                .willReturn(true);
+
+        //when
+        //then
+        ReservationDto.Update updateDto = getUpdateDto(
+                reservationId, resourceId, "newTitle", "newUsage",
+                false, getTime(20), getTime(22)
+        );
+
+        assertThatThrownBy(() -> reservationService.updateReservation(clubMemberId, updateDto))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("수정하려는 시간에 락이 걸려있으면 IllegalStateException")
+    public void updateReservationWhenLockedThenException() throws Exception {
+        //given
+        Long reservationId = 1L;
+        given(lockRepository.existsBetween(any(), any(), eq(resourceId))).willReturn(true);
+
+        //when
+        //then
+        ReservationDto.Update updateDto = getUpdateDto(
+                reservationId, resourceId, "newTitle", "newUsage",
+                false, getTime(20), getTime(22)
+        );
+
+        assertThatThrownBy(() -> reservationService.updateReservation(clubMemberId, updateDto))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("다른 사람의 예약을 수정하려 하면 IllegalStateException")
+    public void updateReservationByOtherClubMemberThenException() throws Exception {
+        //given
+        Long reservationId = 1L;
+        Reservation reservation = createReservation(
+                resource, clubMember, getPeriod(20, 21), "title", "usage", false);
+
+        given(reservationRepository.findById(reservationId)).willReturn(Optional.ofNullable(reservation));
+
+        //when
+        //then
+        ReservationDto.Update updateDto = getUpdateDto(
+                reservationId, resourceId, "newTitle", "newUsage",
+                false, getTime(20), getTime(22)
+        );
+
+        assertThatThrownBy(() -> reservationService.updateReservation(otherClubMemberId, updateDto))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("예약 수정시 예약 정보가 없으면 NoSuchElementException")
+    public void updateReservationWhenNotExistsThenException() throws Exception {
+        //given
+        Long reservationId = 1L;
+        given(reservationRepository.findById(reservationId)).willReturn(Optional.ofNullable(null));
+
+        //when
+        //then
+        ReservationDto.Update updateDto = getUpdateDto(
+                reservationId, resourceId, "newTitle", "newUsage",
+                false, getTime(20), getTime(22)
+        );
+
+        assertThatThrownBy(() -> reservationService.updateReservation(clubMemberId, updateDto))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+
+
+
+    private static LocalDateTime getTime(int hour) {
+        return LocalDateTime.of(2023, 8, 10, hour, 0);
+    }
+
+    private static Period getPeriod(int start, int end) {
+        return new Period(getTime(start), getTime(end));
+    }
+    private Reservation createReservation(Resource resource, ClubMember clubMember, Period period, String title, String usage, boolean sharing) {
+        return Reservation.builder()
+                .resource(resource)
+                .clubMember(clubMember)
+                .period(period)
+                .title(title)
+                .usage(usage)
+                .sharing(sharing)
+                .build();
+    }
+
+    private ReservationDto.Create getCreateDto(
+            Long resourceId, String title, String usage, boolean sharing, LocalDateTime start, LocalDateTime end) {
+
+        return ReservationDto.Create.builder()
+                .resourceId(resourceId)
+                .title(title)
+                .usage(usage)
+                .sharing(sharing)
+                .startDateTime(start)
+                .endDateTime(end)
+                .build();
+    }
+
+    private static ReservationDto.Update getUpdateDto(
+            Long reservationId, Long resourceId, String title, String usage,
+            boolean sharing, LocalDateTime start, LocalDateTime end) {
+
+        return ReservationDto.Update.builder()
+                .reservationId(reservationId)
+                .resourceId(resourceId)
+                .title(title)
+                .usage(usage)
+                .sharing(sharing)
+                .startDateTime(start)
+                .endDateTime(end)
+                .build();
+    }
 }
