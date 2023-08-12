a80aa32b0523e7e0cf2df6834a7eaf96eecb14a2
정찬영 <lms062111@naver.com>
2023-08-12 15:05:05 +0900

    feature : 게시글은 기본적으로 상단 고정된 게시글 → 일반 게시글 순으로 나타나며, 최신순으로 정렬된다.

--- dplanner/src/main/java/com/dp/dplanner/repository/PostRepository.java
+++ dplanner/src/main/java/com/dp/dplanner/repository/PostRepository.java
@@ -2,10 +2,16 @@ package com.dp.dplanner.repository;
 
 import com.dp.dplanner.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findByClubId(Long id);
+    @Query("select p " +
+            "from Post p " +
+            "where p.club.id = :clubId " +
+            "order by p.isFixed desc, p.createdDate desc")
+    List<Post> findByClubId(@Param(value = "clubId") Long clubId);
 }
