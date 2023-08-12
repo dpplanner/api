579fb5cd38a664a06bfaa2ab9a249fe5594182b5
impala <dla4051@naver.com>
2023-08-13 01:06:07 +0900

    feature : ReservationRepository findAllBetween, findAllNotConfirmed 기능 개발

--- dplanner/src/main/java/com/dp/dplanner/repository/ReservationRepository.java
+++ dplanner/src/main/java/com/dp/dplanner/repository/ReservationRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
@@ -38,4 +39,21 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
                                 @Param("resourceId") Long resourceId,
                                 @Param("reservationId") Long reservationId);
 
+
+    @Query("select r " +
+            "from Reservation r " +
+            "where r.resource.id = :resourceId " +
+            "and (r.period.startDateTime <= :start and :start < r.period.endDateTime) " +
+            "or (:start <= r.period.startDateTime and r.period.startDateTime < :end) " +
+            "order by r.period.startDateTime asc , r.period.endDateTime asc")
+    List<Reservation> findAllBetween(@Param("start") LocalDateTime startDateTime,
+                                     @Param("end") LocalDateTime endDateTime,
+                                     @Param("resourceId") Long resourceId);
+
+    @Query("select r " +
+            "from Reservation r " +
+            "where r.resource.id = :resourceId " +
+            "and r.status != 'CONFIRMED' " +
+            "order by r.period.startDateTime asc, r.period.endDateTime asc")
+    List<Reservation> findAllNotConfirmed(@Param("resourceId") Long resourceId);
 }
