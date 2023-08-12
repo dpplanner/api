d1ea311529a3ed2e4d2b92f3a5efb16189a38d96
정찬영 <lms062111@naver.com>
2023-08-12 23:32:00 +0900

    feature : Lock CRUD 할 때 clubMember가 속한 club과 lock의 Resource의 club이 같은지 비교하는 기능 추가

--- dplanner/src/main/java/com/dp/dplanner/service/LockService.java
+++ dplanner/src/main/java/com/dp/dplanner/service/LockService.java
@@ -4,6 +4,8 @@ import com.dp.dplanner.aop.annotation.RequiredAuthority;
 import com.dp.dplanner.domain.Lock;
 import com.dp.dplanner.domain.Period;
 import com.dp.dplanner.domain.Resource;
+import com.dp.dplanner.domain.club.ClubMember;
+import com.dp.dplanner.repository.ClubMemberRepository;
 import com.dp.dplanner.repository.LockRepository;
 import com.dp.dplanner.repository.ResourceRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,15 +24,18 @@ public class LockService {
 
     private final LockRepository lockRepository;
     private final ResourceRepository resourceRepository;
+    private final ClubMemberRepository clubMemberRepository;
 
 
 
     @RequiredAuthority(SCHEDULE_ALL)
     public Response createLock(Long clubMemberId, Create createDto) {
 
-        checkLockDuringPeriod(createDto.getStartDateTime(),createDto.getEndDateTime(), createDto.getResourceId(),null);
+        checkIfThereExistsLocksDuringPeriod(createDto.getStartDateTime(), createDto.getEndDateTime(), createDto.getResourceId(), null);
 
         Resource resource = resourceRepository.findById(createDto.getResourceId()).orElseThrow(RuntimeException::new);
+        checkIfClubMemberAndResourceOfLockIsSameClub(clubMemberId, resource);
+
         Lock lock = lockRepository.save(createDto.toEntity(resource));
 
         return Response.of(lock);
@@ -38,8 +43,12 @@ public class LockService {
 
     @RequiredAuthority(SCHEDULE_ALL)
     public void deleteLock(Long clubMemberId, Long lockId) {
-
         Lock lock = lockRepository.findById(lockId).orElseThrow(RuntimeException::new);
+
+        Resource resource = lock.getResource();
+        checkIfClubMemberAndResourceOfLockIsSameClub(clubMemberId, resource);
+
+
         lockRepository.delete(lock);
     }
 
@@ -47,25 +56,39 @@ public class LockService {
     public Response updateLock(Long clubMemberId, Update updateDto) {
 
         Lock lock = lockRepository.findById(updateDto.getId()).orElseThrow(RuntimeException::new);
-        checkLockDuringPeriod(updateDto.getStartDateTime(),updateDto.getEndDateTime(), updateDto.getResourceId(),lock);
+
+        checkIfThereExistsLocksDuringPeriod(updateDto.getStartDateTime(), updateDto.getEndDateTime(), updateDto.getResourceId(), lock);
+        Resource resource = lock.getResource();
+        checkIfClubMemberAndResourceOfLockIsSameClub(clubMemberId, resource);
+
         lock.update(new Period(updateDto.getStartDateTime(), updateDto.getEndDateTime()));
+
 
         return Response.of(lock);
     }
-    public List<Response> getLocks(Long resourceId, Period period) {
+
+    public List<Response> getLocks(Long clubMemberId, Long resourceId, Period period) {
+
+        Resource resource = resourceRepository.findById(resourceId).orElseThrow(RuntimeException::new);
+        checkIfClubMemberAndResourceOfLockIsSameClub(clubMemberId, resource);
+
         List<Lock> locks = lockRepository.findBetween(period.getStartDateTime(), period.getEndDateTime(), resourceId);
 
         return Response.ofList(locks);
     }
 
+    private void checkIfClubMemberAndResourceOfLockIsSameClub(Long clubMemberId, Resource resource) {
+        ClubMember clubMember = clubMemberRepository.findById(clubMemberId).orElseThrow(RuntimeException::new);
+        if (!clubMember.isSameClub(resource)) {
+            throw new RuntimeException();
+        }
+
+    }
+
     /**
-     *
-     * @param startDateTime
-     * @param endDateTime
-     * @param resourceId
      * @param target : 제외시킬 Lock
      */
-    private void checkLockDuringPeriod(LocalDateTime startDateTime,LocalDateTime endDateTime,Long resourceId,Lock target) {
+    private void checkIfThereExistsLocksDuringPeriod(LocalDateTime startDateTime, LocalDateTime endDateTime, Long resourceId, Lock target) {
         List<Lock> locksBetween = lockRepository.findBetween(startDateTime, endDateTime, resourceId);
 
         if (!locksBetween.stream().filter(lock -> !lock.equals(target)).collect(Collectors.toList()).isEmpty()) {
