f7427730799ef88f58308d889b8b0e19aa0682df
정찬영 <lms062111@naver.com>
2023-08-23 01:14:25 +0900

    feature :  AttachmentRepository findByUrl,deleteByUrl 추가, Attachment Url 유니크 제약 조건 추가

--- dplanner/src/main/java/com/dp/dplanner/repository/AttachmentRepository.java
+++ dplanner/src/main/java/com/dp/dplanner/repository/AttachmentRepository.java
@@ -4,8 +4,13 @@ import com.dp.dplanner.domain.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
 
     List<Attachment> findAttachmentsByPostId(Long postId);
+
+    Optional<Attachment> findByUrl(String url);
+
+    void deleteByUrl(String url);
 }
