7d04191e59240d303487464138ce490db263609e
정찬영 <lms062111@naver.com>
2023-08-24 20:23:38 +0900

    feature : LockController CRUD 추가

---
+++ dplanner/src/main/java/com/dp/dplanner/controller/LockController.java
@@ -0,0 +1,79 @@
+package com.dp.dplanner.controller;
+
+import com.dp.dplanner.aop.annotation.GeneratedClubMemberId;
+import com.dp.dplanner.domain.Period;
+import com.dp.dplanner.service.LockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.dp.dplanner.dto.LockDto.*;
+
+@RestController
+@RequiredArgsConstructor
+public class LockController {
+
+    private final LockService lockService;
+
+    @PostMapping(value = "/locks/resources/{resourceId}", params = "clubId")
+    public ResponseEntity<Response> createLock(@GeneratedClubMemberId Long clubMemberId,
+                                               @RequestParam Long clubId,
+                                               @PathVariable Long resourceId,
+                                               @RequestBody Create createDto) {
+        Response response = lockService.createLock(clubMemberId, createDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
+
+    }
+
+    @GetMapping(value = "/locks/resources/{resourceId}", params = "clubId")
+    public ResponseEntity<List<Response>> getLocks(@GeneratedClubMemberId Long clubMemberId,
+                                                   @RequestParam Long clubId,
+                                                   @PathVariable Long resourceId,
+                                                   @RequestBody Request request) {
+
+        List<Response> response = lockService.getLocks(clubMemberId, resourceId, new Period(request.getStartDateTime(),request.getEndDateTime()));
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+    }
+
+    @GetMapping(value = "/locks/{lockId}", params = "clubId")
+    public ResponseEntity<Response> getLock(@GeneratedClubMemberId Long clubMemberId,
+                                               @RequestParam Long clubId,
+                                               @PathVariable Long lockId) {
+
+        Response response = lockService.getLock(clubMemberId,lockId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+
+    }
+
+    @PutMapping(value = "/locks/{lockId}", params = "clubId")
+    public ResponseEntity<Response> updateLock(@GeneratedClubMemberId Long clubMemberId,
+                                               @RequestParam Long clubId,
+                                               @PathVariable Long lockId,
+                                               @RequestBody Update updateDto) {
+
+        Response response = lockService.updateLock(clubMemberId, updateDto);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+
+    }
+
+    @DeleteMapping(value = "/locks/{lockId}", params = "clubId")
+    public ResponseEntity deleteLock(@GeneratedClubMemberId Long clubMemberId,
+                                     @RequestParam Long clubId,
+                                     @PathVariable Long lockId) {
+
+        lockService.deleteLock(clubMemberId, lockId);
+
+        return ResponseEntity.noContent().build();
+    }
+}
