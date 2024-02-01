dd1242ff9c05b6a27e782ab0caedb5b0837639ac
Chan Young <lms062111@naver.com>
2024-02-01 22:43:16 +0900

    cicd : git web hook 동작 test

--- dplanner/src/main/java/com/dp/dplanner/DplannerApplication.java
+++ dplanner/src/main/java/com/dp/dplanner/DplannerApplication.java
@@ -3,7 +3,7 @@ package com.dp.dplanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+//
 @EnableJpaAuditing
 @SpringBootApplication
 public class DplannerApplication {
