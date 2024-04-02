1a4ef3177a0f07925ac2ca7e1f5d7a2aa171b566
Chan Young <lms062111@naver.com>
2024-04-02 22:56:27 +0900

    feature : Member 생성, 수정 시간 컬럼 추가

--- dplanner/src/main/java/com/dp/dplanner/domain/Member.java
+++ dplanner/src/main/java/com/dp/dplanner/domain/Member.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue
