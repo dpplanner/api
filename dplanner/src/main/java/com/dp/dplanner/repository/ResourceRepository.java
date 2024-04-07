8921ae3f67fe9b2799ac32eaebb4ee5609798296
Chan Young <lms062111@naver.com>
2024-04-07 23:01:36 +0900

    feature : 클럽 자원 목록 정렬 조건 추가

--- dplanner/src/main/java/com/dp/dplanner/repository/ResourceRepository.java
+++ dplanner/src/main/java/com/dp/dplanner/repository/ResourceRepository.java
@@ -2,11 +2,19 @@ package com.dp.dplanner.repository;
 
 import com.dp.dplanner.domain.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ResourceRepository extends JpaRepository<Resource,Long> {
 
-    List<Resource> findByClubId(Long clubId);
+    @Query("""
+            SELECT r
+            FROM Resource r
+            WHERE r.club.id = :clubId
+            ORDER BY r.name
+            """)
+    List<Resource> findByClubId(@Param(value = "clubId") Long clubId);
 
 }
