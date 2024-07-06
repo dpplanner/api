ab109657fe269623b83211af4ede8c275d16ea78
Chan Young <lms062111@naver.com>
2024-07-06 20:46:25 +0900

    edit: create_table 컬럼 중요도 순서대로 순서 변경

--- dplanner/deploy/create_tables.sql
+++ dplanner/deploy/create_tables.sql
@@ -1,12 +1,12 @@
 -- Create Tables
 create table attachment (
     id bigint not null,
-    created_date timestamp(6),
-    last_modified_date timestamp(6),
     type varchar(255) not null check (type in ('VIDEO','IMAGE','NONE')),
     url varchar(255),
     post_id bigint,
     reservation_id bigint,
+    created_date timestamp(6),
+    last_modified_date timestamp(6),
     primary key (id)
 );
 
@@ -20,9 +20,9 @@ create table club (
 
 create table club_authority (
     id bigserial not null,
-    description varchar(255),
-    name varchar(255),
     club_id bigint,
+    name varchar(255),
+    description varchar(255),
     primary key (id)
 );
 
@@ -33,28 +33,28 @@ create table club_authority_club_authority_types (
 
 create table club_member (
     id bigserial not null,
-    info varchar(255),
-    is_confirmed boolean,
-    is_deleted boolean,
-    name varchar(255),
-    role varchar(255) not null check (role in ('ADMIN','MANAGER','USER','NONE')),
-    url varchar(255),
     club_id bigint,
     club_authority_id bigint,
     member_id bigint,
+    name varchar(255),
+    role varchar(255) not null check (role in ('ADMIN','MANAGER','USER','NONE')),
+    info varchar(255),
+    is_confirmed boolean,
+    is_deleted boolean,
+    url varchar(255),
     primary key (id)
 );
 
 create table comment (
     id bigint not null,
-    created_date timestamp(6),
-    last_modified_date timestamp(6),
-    content varchar(255),
-    is_deleted boolean,
     club_id bigint,
     club_member_id bigint,
-    parent_id bigint,
     post_id bigint,
+    parent_id bigint,
+    content varchar(255),
+    is_deleted boolean,
+    created_date timestamp(6),
+    last_modified_date timestamp(6),
     primary key (id)
 );
 
@@ -67,54 +67,55 @@ create table comment_member_like (
 
 create table comment_report (
     id bigint not null,
-    message varchar(255),
     club_member_id bigint,
     comment_id bigint,
+    message varchar(255),
     primary key (id)
 );
 
 create table invite_code (
     id bigint not null,
+    club_id bigint,
+    code varchar(255),
     created_date timestamp(6),
     last_modified_date timestamp(6),
-    code varchar(255),
-    club_id bigint,
     primary key (id)
 );
 
 create table lock (
     id bigserial not null,
+    resource_id bigint,
+    message varchar(255),
+    start_date_time timestamp(6),
+    end_date_time timestamp(6),
     created_date timestamp(6),
     last_modified_date timestamp(6),
-    message varchar(255),
-    end_date_time timestamp(6),
-    start_date_time timestamp(6),
-    resource_id bigint,
     primary key (id)
 );
 
 create table member (
     id bigint not null,
+    recent_club_id bigint,
+    email varchar(255),
+    name varchar(255),
+    is_deleted boolean,
+    fcm_token varchar(255),
+    refresh_token varchar(255),
+    eula boolean default false,
     created_date timestamp(6),
     last_modified_date timestamp(6),
-    email varchar(255),
-    fcm_token varchar(255),
-    is_deleted boolean,
-    name varchar(255),
-    refresh_token varchar(255),
-    recent_club_id bigint,
     primary key (id)
 );
 
 create table post (
     id bigint not null,
-    created_date timestamp(6),
-    last_modified_date timestamp(6),
-    content text,
-    is_fixed boolean,
-    title varchar(255),
     club_id bigint,
     club_member_id bigint,
+    title varchar(255),
+    content text,
+    is_fixed boolean,
+    created_date timestamp(6),
+    last_modified_date timestamp(6),
     primary key (id)
 );
 
@@ -127,42 +128,42 @@ create table post_member_like (
 
 create table post_report (
     id bigint not null,
-    message varchar(255),
     club_member_id bigint,
     post_id bigint,
+    message varchar(255),
     primary key (id)
 );
 
 create table private_message (
     id bigint not null,
-    created_date timestamp(6),
-    last_modified_date timestamp(6),
+    club_member_id bigint,
+    title varchar(255),
     content varchar(255),
-    info varchar(255),
+    type varchar(255) not null check (type in ('ACCEPT','INFO','NOTICE','REJECT','REPORT','REQUEST')),
     info_type varchar(255) not null check (info_type in ('POST','RESERVATION','MEMBER','RETURN')),
+    info varchar(255),
     is_read boolean,
     redirect_url varchar(255),
-    title varchar(255),
-    type varchar(255) not null check (type in ('ACCEPT','INFO','NOTICE','REJECT','REPORT','REQUEST')),
-    club_member_id bigint,
+    created_date timestamp(6),
+    last_modified_date timestamp(6),
     primary key (id)
 );
 
 create table reservation (
     id bigserial not null,
-    created_date timestamp(6),
-    last_modified_date timestamp(6),
-    is_returned boolean not null,
-    end_date_time timestamp(6),
-    start_date_time timestamp(6),
-    reject_message varchar(255),
-    return_message varchar(255),
-    sharing boolean not null,
-    status varchar(255) check (status in ('REQUEST','CONFIRMED','REJECTED')),
-    title varchar(255),
-    usage varchar(255),
     club_member_id bigint,
     resource_id bigint,
+    start_date_time timestamp(6),
+    end_date_time timestamp(6),
+    title varchar(255),
+    usage varchar(255),
+    status varchar(255) check (status in ('REQUEST','CONFIRMED','REJECTED')),
+    sharing boolean not null,
+    is_returned boolean not null,
+    reject_message varchar(255),
+    return_message varchar(255),
+    created_date timestamp(6),
+    last_modified_date timestamp(6),
     primary key (id)
 );
 
@@ -175,23 +176,24 @@ create table reservation_invitee (
 
 create table resource (
     id bigserial not null,
+    club_id bigint,
+    name varchar(255),
+    resource_type varchar(255) not null check (resource_type in ('PLACE','THING')),
+    bookable_span bigint,
+    return_message_required boolean not null,
+    info varchar(255),
+    notice text,
     created_date timestamp(6),
     last_modified_date timestamp(6),
-    bookable_span bigint,
-    info varchar(255),
-    name varchar(255),
-    notice text,
-    resource_type varchar(255) not null check (resource_type in ('PLACE','THING')),
-    return_message_required boolean not null,
-    club_id bigint,
     primary key (id)
 );
 
-create table posb_block (
+create table post_block (
     club_member_id bigint not null,
     post_id bigint not null,
     primary key (club_member_id, post_id)
 );
+
 
 -- Add Constraints
 alter table if exists attachment
